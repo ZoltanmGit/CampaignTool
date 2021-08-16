cbf051227de5941851f97b6fd6b2081d78d9056c
ZoltanmGit <zoltanm.private@gmail.com>
2021-08-16 20:34:16 +0200

    Started Working on GameModeBase

--- Source/CampaignTool/CampaignToolGameModeBase.h
+++ Source/CampaignTool/CampaignToolGameModeBase.h
@@ -13,5 +13,7 @@ UCLASS()
 class CAMPAIGNTOOL_API ACampaignToolGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Init)
+		TSubclassOf<class AMovementController> MovementController;
 };
