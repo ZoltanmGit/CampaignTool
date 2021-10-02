e3392c293f06f5c94301c209d3c7aa5496627336
ZoltanmGit <zoltanm.private@gmail.com>
2021-10-02 16:44:24 +0200

    Started working on NewCharacter UI

---
+++ Source/CampaignTool/Public/NewCharacterGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NewCharacterGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAMPAIGNTOOL_API ANewCharacterGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
