d0d9d15f7936924f1efb9d5aabd62c18b1aab0d5
ZoltanmGit <zoltanm.private@gmail.com>
2021-11-16 18:27:35 +0100

    Started Items

---
+++ Source/CampaignTool/Public/ItemStructures.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "ItemStructures.generated.h"
+
+USTRUCT(BlueprintType)
+struct FItemProperties : public FTableRowBase
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = ItemProp)
+		FString ItemCode;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = ItemProp)
+		FString Name;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = ItemProp)
+		FString Description;
+
+};
